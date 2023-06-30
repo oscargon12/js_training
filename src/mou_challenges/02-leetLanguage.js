5c71e652696d6f2d4f9091b385ce562245c333c9
oscargon12 <oscargon12@gmail.com>
2023-06-30 01:00:36 -0500

    leet language

--- src/mou_challenges/02-leetLanguage.js
+++ src/mou_challenges/02-leetLanguage.js
@@ -10,22 +10,40 @@
 const regularAlphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 
 ]
 
 const leetAlphabet = [
-    '4', 'I3', '[', 'I)', '3', 'I=', '&', '#', '1',
-    ',_i', 'i<', '1', 'jvi', 'I/I', '0', 'I°', '(_,)',
-    'I2', '5', '7', '(_)', '\/', '\/\/', '><', 'J', '2', 
+    '4', 'I3', '[', 'I>', '3', 'I=', '&', 'I-I', '1',
+    ',_i', 'i<', '|', '/VL', 'L/I', '0', 'I°', '(_,)',
+    '|2', '5', '7', '(_)', 'L/', 'L/L/', '><', 'J', '2', ' ',
 ]
 
-//let myPhrase = 'hola mundo'
+let myPhrase = 'Oscar'
 
-const leetLanguage = () =>{
-    if(regularAlphabet.length === leetAlphabet.length){
-        console.log('Si son las mismas letras');
+const leetLanguage = ( phrase ) =>{
+    if(regularAlphabet.length === leetAlphabet.length && //Valida que sean la misma cantidad de letras
+        regularAlphabet[regularAlphabet.length - 1] === leetAlphabet[leetAlphabet.length - 1]){ //Valida que el ultimo caracter sea espacio
+        console.log('Frase original: ', phrase)
+        console.log('traduciendo ...');
+        
+        let userPhrase = phrase.toLowerCase();
+        let result = '';
+
+        for(let i = 0; i <= userPhrase.length - 1; i++){ //Un for que recorra la frase de usuario
+            let char = userPhrase[i] //Capturo los caracteres de la frase
+            let indexChar = regularAlphabet.indexOf(char); //Capturo la posición de esos caracteres en regularAlphabet
+            console.log(`${char} ${indexChar}`) 
+
+            result += leetAlphabet[indexChar]; //result = result + cada posicion pero del otro alfabeto
+        }
+        return result;
+        
+    } 
+    else {
+        console.log('Debes revisar la configuración de tu abecedario')
     }
 }
 
-leetLanguage();
+console.log(leetLanguage(myPhrase));
 
